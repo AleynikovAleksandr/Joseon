c0b7ec8f6108e04a79a5508a4f91b8fb859484e1
AleynikovAleksandr <aleynikov.aleksandr@icloud.com>
2024-11-19 22:47:58 +1000

    Add files via upload

---
+++ WpfApp6/WpfApp6/Window1.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1(int correctAnswers, string correctQuestions, string wrongQuestions)
+        {
+            InitializeComponent();
+
+            
+            string resultMessage = $"{correctQuestions}\n{wrongQuestions}";
+
+
+            if (correctAnswers == 4)
+            {
+                resultMessage += "\nХорошо!";
+            }
+            else if (correctAnswers == 2 || correctAnswers == 3)
+            {
+                resultMessage += "\nНеплохо!";
+            }
+            else if (correctAnswers == 0 || correctAnswers == 1)
+            {
+                resultMessage += "\nПлохо!";
+            }
+
+            ResultText.Text = resultMessage;
+        }
+
+        
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
